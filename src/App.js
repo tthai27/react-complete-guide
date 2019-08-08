a8c471079fbf51e54635513b70887afac5391c82
Tony <tthai27@gmail.com>
2019-08-08 11:26:33 -0400

    complete section 3

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, {Component} from 'react';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+
   state = {
-    persons: [
-      { name: 'Tony', age: 31 },
-      { name: 'Stella', age: 30 },
-      { name: 'Bryan', age: 2 },
-      { name: 'Madison', age: 4 }
-    ]
-  }
-  
-  switchNameHandler = () => {
-    console.log("clicked");
-    this.setState({
-      persons: [
-        { name: 'Tony', age: 32 },
-        { name: 'Stella', age: 30 },
-        { name: 'Bryan', age: 2 },
-        { name: 'Madison', age: 4 }
-      ]
-    })
+    username: 'supertony'
   }
 
-  render() {
+  usernameChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+   render() {
     return (
       <div className="App">
-          <h1>Hi Im</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/> onC
+         <UserInput
+          changed={this.usernameChangeHandler} 
+         currentName={this.state.username}/>
+         <UserOutput userName={this.state.username}/>
+         <UserOutput userName={this.state.username}/>
+         <UserOutput userName={this.state.username}/>
+         <UserOutput userName="Max"/>
+         <UserOutput userName="Max"/>
       </div>
-    );
+     
+ 
+      );
   }
+
 }
 
 export default App;
+
+
+
+
